56a40e59991753a51ca2b4a4cada81c1945c0c1e
Quinn Slack <quinn@slack.org>
2022-12-27 22:12:09 -1000

    fix handling of additionalProperties when there are >1 explicit properties (#17)
    When an object type had 2 or more explicitly defined properties, the generated code for the Go type's UnmarshalJSON method was syntactically invalid due to a whitespace bug.
    
    Also, the test for the object-with-props test case was not being executed because it was underneath a directory named `testdata`, so it was moved to the `compiler` package.
    
    Finally, there are a few other optimizations to the generated code.

---
+++ compiler/generator_object_additional_properties_test.go
@@ -0,0 +1,34 @@
+package compiler
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+
+	objectwithprops "github.com/sourcegraph/go-jsonschema/compiler/testdata/object-with-props"
+)
+
+func TestAdditionalProperties(t *testing.T) {
+	o := objectwithprops.ObjectWithProps{
+		A:          "1",
+		B:          "2",
+		Additional: map[string]interface{}{"c": "3", "d": "4"},
+	}
+
+	data, err := json.Marshal(o)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if want := `{"a":"1","b":"2","c":"3","d":"4"}`; string(data) != want {
+		t.Errorf("got %s, want %s", data, want)
+	}
+
+	var o2 objectwithprops.ObjectWithProps
+	if err := json.Unmarshal(data, &o2); err != nil {
+		t.Fatal(err)
+	}
+
+	if !reflect.DeepEqual(o, o2) {
+		t.Errorf("got %+v, want %+v", o, o2)
+	}
+}
